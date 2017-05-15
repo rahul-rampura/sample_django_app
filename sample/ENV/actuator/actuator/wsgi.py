8980f2ef4ee520a6bf226fb2c9d09db03a81688b
rrampura <rahul.rampura@coriolis.co.in>
2017-05-15 18:08:28 +0530

    Initial commit

---
+++ sample/ENV/actuator/actuator/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for actuator project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "actuator.settings")
+
+application = get_wsgi_application()
